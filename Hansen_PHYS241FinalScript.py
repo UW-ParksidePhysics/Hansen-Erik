aef212877e79708c123a4951a353199317b2c148
hanse109 <60672021+hanse109@users.noreply.github.com>
2020-05-14 21:34:44 -0500

    Add files via upload

--- Hansen_PHYS241FinalScript.py
+++ Hansen_PHYS241FinalScript.py
@@ -1,37 +1,91 @@
-from Final_exam.two_column_text_read_11 import read_data, x1, y1
+from Final_exam.two_column_text_read_11 import x1, y1
 from Final_exam.quadratic_fit_11 import quadratic_fit
 from Final_exam.univariate_statistics_11 import univariate_statistics
-import matplotlib.pyplot as plt
-from Final_exam.fit_curve_array_11 import array_data, fit_curve, x
+from Final_exam.fit_curve_array_11 import x
 import Final_exam.plot_data_with_fit_11
+import matplotlib.pyplot as plt
 from Final_exam.equations_of_state import fit_eos
-import numpy as np
+from numpy import *
 
 """Read in the data into a NumPy array using your two_column_text_read function"""
 """Pull out the statistics on the data set using your univariate_statistics function """
 """Fit a quadratic polynomial to the data using your quadratic_fit function """
 
 
-def parse_file_name():
-    return print('Pb, Fm-3m, GGA-PBE')
+def parse_file_name():      # part 1
+    file_name = 'Pb.Fm-3m.GGA-PBE.volumes_energies.dat.txt'
+    symbol = file_name.split('.')[0]
+    symmetry = file_name.split('.')[1]
+    density = file_name.split('.')[2]
+    return symbol, symmetry, density
 
 
 """ Extract the chemical symbol, crystal symmetry symbol, density functional exchange-correlation
 approximation acronym from your give data file's name"""
 
-"""def convert_units():
-    x2 = 1.48185e-61 * x  # bohr^3 to angstrom^3
-    y2 = 13.6056980659 * y  # rydberg to ev
-    z2 = y2/x2
-    return x2, y2, z2
+print('Stats:', univariate_statistics())  # pulling out the stats, part 3
 
-COULDN'T FIGURE OUT HOW TO CONVERT PROPERLY SO I DID IT OUTSIDE OF PYTHON"""
+"""Pull out the statistics on the data set using your univariate_statistics function from the Final Review;"""
 
-print('Stats:', univariate_statistics())  # printing the stats
-fit_eos(y1, x1, quadratic_fit(), eos='vinet')
-plt.xlim([min(x1) * .9, max(x1) * 1.1])
-plt.xlabel('E(eV/atom)')
-plt.ylabel('V(Å3/atom)')
-plt.plot(fit_eos)
-plt.show()
+quadratic_fit()
+
+"""Fit a quadratic polynomial to the data using your quadratic_fit function from the Final Review"""
+
+fit_eos(x1, y1, quadratic_fit(), eos='vinet')
+
+"""Pass the fit quadratic coefficients and the data to the fit_eos function linked through the
+Final Exam Data page, passing the parameters assigned to you"""
+
+
+"""def convert_units(file_name):
+    with open(file_name, 'r') as data:
+        x = []
+        y = []
+        for line in data:
+            x.append(1.48185e-61 * float(line[0]))  # bohr^3 to angstrom^3
+            y.append(13.6056980659 * float(line[1]))  # rydberg to ev
+            return x, y
+
+
+(x, y) = convert_units('Pb.Fm-3m.GGA-PBE.volumes_energies.dat.txt')"""
+
+"""Convert that data and fit from atomic units,
+ to these units using a module you write called convert_units:"""
+
+
+"""plt.plot(fit_eos)"""
+plt.xlim([min(x1) * .9, max(x1) * 1.1])     # generating a plot based off volume_energies data given
+plt.ylabel('E(eV/atom)')
+plt.xlabel('V(Å3/atom)')
+
+"""Plot the data and the fit function with volume on the horizontal axis and 
+energy on the vertical axis:"""
+
+
+def annotate_graph():
+    plt.text(90, -11844.40922, 'Pb')
+    plt.text(111, -11844.50922, 'Fm-3m')
+    plt.text(111, -11844.48922, 'GGA-PBE')
+    plt.axvline(x=100, ymin=-11844.51765, ymax=-11844.4, color='k', label='equillibium volume')
+    plt.text(87, -11844.525, 'Created by Erik Hansen 2020-05-12')
+    return
+
+
+"""Write a function called annotate_graph"""
+
+
+annotate_graph()
+
+display_graph = 'tru'
+if display_graph == 'true':
+    plt.show()
+else:
+    plt.savefig('Hansen.Pb.Fm-3m.GGA-PBE.VinetEquationOfState.png')
+
+"""Finally, declare (in the parameters section of the script) a variable called display_graph, 
+which is a Boolean value that calls PyPlot's show function when True, and savefig when False"""
+
+
+"""This wasn't my best attempt at the code, but I decided to submit what worked and created a graph.
+I struggled getting all the data through the convert and equation of state without getting an error"""
 
--- Hansen_PHYS241FinalScript_Matrix.py
+++ Hansen_PHYS241FinalScript_Matrix.py
@@ -1,5 +1,5 @@
 from Final_exam.generate_matrix import generate_matrix
-from numpy import asarray, linalg, linspace
+from numpy import asarray, linalg, linspace, reshape
 import matplotlib.pyplot as plt
 
 
@@ -10,14 +10,20 @@ array = asarray(x)   # convert data to array
 w.sort()   # change eigenvalues to ascending order
 v = u[::-1]
 n_dim = linspace(-10, 10, 120)
-plt.plot(n_dim, u)
+plt.plot(n_dim, v)
 plt.axhline(y=0, color='k', label='Created by Erik Hansen 2020-05-12')
 plt.xlabel('x[a.u.]')
 plt.ylabel('y[a.u.]')
 plt.ylim(-.5, .5)
 plt.legend(loc='lower left')
 plt.title('Select Wavefunctions for a Sinusoidal Potential on a Spatial Grid of 120 Points')
-plt.show()
+plt.text(-7.5, .4, 'Couldnt get it to only print 1, 2, 3')
+display_graph = 'true'
+if display_graph == 'true':
+    plt.show()
+else:
+    plt.savefig('Sinusiodal.Eigenvector(1_2_3).png')
+
 """print('Eigenvalues', w[1:4])
 print('Eigenvectors', v[1:4])"""
 
